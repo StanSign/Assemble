7102eb7eea7defe4a7505f37dcb0e1bb9298938c
Stansign <crystalashe@naver.com>
2022-04-14 17:13:01 +0900

    [Feat] 디자인 통일을 위한 Font Size enum 생성

---
+++ Assemble/Util/Constant/TypeSize.swift
@@ -0,0 +1,21 @@
+//
+//  TypeSize.swift
+//  Assemble
+//
+//  Created by 이창준 on 2022/04/14.
+//
+
+import Foundation
+
+enum TypeSize: Int {
+    case largeTitle = 34
+    case title_01 = 28
+    case title_02 = 22
+    case title_03 = 20
+    case body = 17
+    case callout = 16
+    case subhead = 15
+    case footnote = 13
+    case caption_1 = 12
+    case caption_2 = 11
+}
