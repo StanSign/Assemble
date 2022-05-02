ef4723ddddcdd0149deb3a970c898c31c4bdc390
Stansign <crystalashe@naver.com>
2022-05-02 16:05:54 +0900

    [Chore] 뉴스 Mock 서버 주소 추가

---
+++ Assemble/Util/Constant/MockServerConstants.swift
@@ -0,0 +1,12 @@
+//
+//  MockServerConstants.swift
+//  Assemble
+//
+//  Created by 이창준 on 2022/05/02.
+//
+
+enum MockServerConstants {
+    static let baseURL = "https://c34acec2-303f-4243-8b23-bd1fc8acd4c5.mock.pstmn.io"
+    static let upcomingFilmPath = "/films/upcoming"
+    static let newsPath = "/news"
+}
